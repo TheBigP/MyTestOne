cbdb4a9ab213c0d2b6148480e6853d19515b47f4
TheBigP <33213320+TheBigP@users.noreply.github.com>
2017-11-29 17:32:06 +0800

    react

--- react-example/my-app/src/App.js
+++ react-example/my-app/src/App.js
@@ -74,6 +74,27 @@ handlerSearch(){
       </tbody>
       </table>
     </div>
+
+
+    // let items = [];
+
+    // if(this.props.items.length == 0) {
+    //     items.push(<tr><th colSpan="5" className="tempEmpty">暂无用户</th></tr>);
+    // }else {
+    //     this.props.items.forEach(item => {
+    //         items.push(<StaffItem key={item.key} item={item}/>);
+    //     });
+    // }
+    // <table className='itemPanel'>
+      // <thead>
+          // <th className='itemTd'>姓名</th>
+          // <th className='itemTd'>年龄</th>
+          // <th className='itemTd'>身份</th>
+          // <th className='itemTd'>性别</th>
+          // <th className='itemTd'>操作</th>
+      // </thead>
+      // <tbody>{items}</tbody>
+    // </table>
   );
 }
 
