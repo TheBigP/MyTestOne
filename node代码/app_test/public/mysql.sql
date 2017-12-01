1265f0e97f56f02c1ae6bdda132e8f731210542a
TheBigP <33213320+TheBigP@users.noreply.github.com>
2017-12-01 17:33:50 +0800

    login
    后续没做完

---
+++ node代码/app_test/public/mysql.sql
@@ -0,0 +1,6 @@
+create table admin (
+    -> admin_id varchar(10),admin_name varchar(255),admin_psd varchar(255),
+    -> admin_title varchar(255),admin_titleid varchar(11),admin_department varchar(255),
+    -> admin_super int(255));
+
+    insert into admin values('000001','miss H','123','辅导员','13154','金融系',000);
--- node代码/app_test/routes/index.js
+++ node代码/app_test/routes/index.js
@@ -1,6 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var usr=require('dao/Connect');
+router.post('/',function(req,res){
+  client = usr.connectServer();
+  var admin_id =req.body.admin_id;
+  var admin_psd=req.body.admin_psd;
+  usr.loginFun(client,admin_id,function(results){
+    if(results==''){
+      res.locals.error='用户不存在';
+      res.render('index',{title:'smartCampus'});
+      return;
+    }else{
+      if(results[0].admin_psd==admin_psd){
+        res.locals.islogin=admin_id;
+        res.cookie('islogin',res.locals.islogin,{maxAge:6000});
+        res.redirect('success');
+        return;
+      }else{
+        res.locals.error='密码错误';
+        res.render('index',{title:'smartCampus'});
+        retrun;
+      }
+    }
+  });
+});
 
+router.get('/success',function(req,res,next){
+  res.send('登陆成功');
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
